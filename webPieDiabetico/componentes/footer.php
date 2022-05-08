d41c47c4fcba2a3258c57ec36ac316763df676bc
Renzo Palomino <renzo.palominop@gmail.com>
2022-05-08 17:42:42 -0500

    html to PHP

---
+++ webPieDiabetico/componentes/footer.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="css/main.css" rel="stylesheet">
+    <script src="https://code.iconify.design/2/2.2.1/iconify.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+    <nav class="navbar bg-dark navbar-dark">
+        <div class="container-fluid row" >
+        <div class="col-6">
+            <span style="color: #f8f8f8; font-size: 50px;">LOGO</span>
+        </div>
+        <div class="col-6 d-flex justify-content-end">
+            <span class="iconify" data-icon="ant-design:facebook-outlined" style="color: #f8f8f8; font-size: 50px;"></span>
+            <span class="iconify" data-icon="akar-icons:instagram-fill" style="color: #f8f8f8; font-size: 50px;"></span>
+        </div>
+        </div>
+    </nav>
+
+</body>
+</html>
