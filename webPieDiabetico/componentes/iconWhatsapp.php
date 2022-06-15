75b72ee8d3e1c821b4c68a41736b67a6a6a2f4f9
Renzo Palomino <renzo.palominop@gmail.com>
2022-06-14 23:30:23 -0500

    retoques

--- webPieDiabetico/componentes/iconWhatsapp.php
+++ webPieDiabetico/componentes/iconWhatsapp.php
@@ -1,5 +1,5 @@
     <div>
-        <a type="button" class="btn-float" href="https://wa.me/+51">
+        <a type="button" class="btn-float" href="https://wa.me/+51940959522" target="_blank">
             <i class="iconify" data-icon="logos:whatsapp" style="color: #f8f8f8; font-size: 50px;"></i>
         </a>
     </div>
--- webPieDiabetico/tratamiento.php
+++ webPieDiabetico/tratamiento.php
@@ -1,1 +1,209 @@
-Amor de mi corazon
+<!doctype html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <?php include 'componentes/referencias.php' ?>
+    <title>Tratamiento</title>
+  </head>
+  <body style="background: #96c11f;">
+<!--NAVBAR-->
+<?php include 'componentes/navbar.php' ?>
+<!--ENDNAVBAR-->
+
+<div class="navbar justify-content-center text-negrita-3 py-5">Tratamiento</div>
+
+<div class="container">
+  <div class="card-columns pb-5">
+    <div class="card-2 py-4 " type="button" data-bs-toggle="modal" data-bs-target="#myModal">
+      <div class="card-body text-center">
+        <p class="card-text">Ozonoterapia en bolsa</p>
+      </div>
+    </div>
+    <div class="card-2 py-4 " type="button" data-bs-toggle="modal" data-bs-target="#myModa4">
+      <div class="card-body text-center">
+        <p class="card-text">Podología especializada</p>
+      </div>
+    </div>
+    <div class="card border border-0">
+      
+    </div>
+    <div class="card-2 py-4 " type="button" data-bs-toggle="modal" data-bs-target="#myModa2">
+      <div class="card-body text-center">
+        <p class="card-text">Aceite ozonizado</p>
+      </div>
+    </div>  
+    <div class="card-2 py-4 " type="button" data-bs-toggle="modal" data-bs-target="#myModa5">
+      <div class="card-body text-center">
+        <p class="card-text">Ortopedia</p>
+      </div>
+    </div>
+    <div class="card-2 py-4 " type="button" data-bs-toggle="modal" data-bs-target="#myModa7">
+      <div class="card-body text-center">
+        <p class="card-text">Crema hidratante para pie diabético</p>
+      </div>
+    </div>
+    <div class="card-2 py-4 " type="button" data-bs-toggle="modal" data-bs-target="#myModa3">
+      <div class="card-body text-center">
+        <p class="card-text">Sistema cierre asistido al vacio (VAC)</p>
+      </div>
+    </div>
+    <div class="card-2 py-4 " type="button" data-bs-toggle="modal" data-bs-target="#myModa6">
+      <div class="card-body text-center">
+        <p class="card-text">Plasma rico en plaquetas</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- The Modal1 -->
+<div class="modal" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal1 Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Ozonoterapia en bolsa</h4>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+      </div>
+
+      <!-- Modal1 body -->
+      <div class="modal-body">
+        <img src="/img/Tratamiento/ozonoterapia.png" style="width: 65vw;">
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<!-- The Modal2 -->
+<div class="modal" id="myModa2">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal1 Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Aceite ozonizado</h4>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+      </div>
+
+      <!-- Modal1 body -->
+      <div class="modal-body">
+        <img src="/img/Tratamiento/aceiite.png" style="width: 65vw;">
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<!-- The Modal3 -->
+<div class="modal" id="myModa3">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal1 Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Sistema cierre asistido al vacio (VAC)</h4>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+      </div>
+
+      <!-- Modal1 body -->
+      <div class="modal-body">
+        <img src="/img/Tratamiento/sistemaVacio.png" style="width: 65vw;">
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<!-- The Modal4 -->
+<div class="modal" id="myModa4">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal1 Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Podología especializada</h4>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+      </div>
+
+      <!-- Modal1 body -->
+      <div class="modal-body">
+        <img src="/img/Tratamiento/podologia.png" style="width: 65vw;">
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<!-- The Modal5 -->
+<div class="modal" id="myModa5">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal1 Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Ortopedia</h4>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+      </div>
+
+      <!-- Modal1 body -->
+      <div class="modal-body">
+        <img src="/img/Tratamiento/ortopedia.png" style="width: 65vw;">
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<!-- The Modal6 -->
+<div class="modal" id="myModa6">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal1 Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Plasma rico en plaquetas</h4>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+      </div>
+
+      <!-- Modal1 body -->
+      <div class="modal-body">
+        <img src="/img/Tratamiento/plasmaPlaquetas.png" style="width: 65vw;">
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<!-- The Modal7 -->
+<div class="modal" id="myModa7">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal1 Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Crema hidratante para pie diabético</h4>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+      </div>
+
+      <!-- Modal1 body -->
+      <div class="modal-body">
+        <img src="/img/Tratamiento/cremaHidratante.png" style="width: 65vw;">
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<!--iconWhatsapp-->
+<?php include 'componentes/iconWhatsapp.php' ?>
+  <!--Footer-->
+  <?php include 'componentes/footer.php' ?>
+
+
+  </body>
+</html>
